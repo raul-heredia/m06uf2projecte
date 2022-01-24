7c469ccf13f7d2f4bbc621662ea22dc80513e348
Raúl Heredia <raul.heredia01@outlook.com>
2022-01-24 17:25:39 +0100

    Genera taula desde dispatch de redux falten inputs

--- index.js
+++ index.js
@@ -5,9 +5,9 @@ function main() {
     const reducer = (state = [], action) => {
         console.log("reducer", state, action);
         if (action.type === "ADD_USER") {
-            return [...state, action.username];
+            return [...state, [action.nom, action.curs, action.modul, action.nota]];
         };
-        return state
+        return state;
     };
     const store = Redux.createStore(reducer);
 
@@ -21,23 +21,39 @@ function main() {
     // store.dispatch({ type: "ADD_USER", username: "john" });
 
 
-    const list = document.getElementById('list');
+    const taula = document.getElementById('taula');
     const addUserBtn = document.getElementById('addUser');
     const userInput = document.getElementById('userInput');
 
     store.subscribe(() => {
-        list.innerHTML = '';
+        taula.innerHTML = '<thead> <tr> <th scope="col">Nom</th> <th scope="col">Curs</th> <th scope="col">Modul</th> <th scope="col">Nota</th> </tr> </thead><tbody>';
         userInput.value = '';
+
         store.getState().forEach(user => {
-            const li = document.createElement('li');
-            li.className = 'list-group-item'
-            li.textContent = user;
-            list.appendChild(li);
+            let fila = document.createElement('tr');
+            for (let i in user) {
+                let cela = document.createElement('td');
+                cela.textContent = user[i];
+                if (i == 3) {
+                    if (user[i] < 5) {
+                        cela.classList = 'suspes';
+                    } else if (user[i] == 5) {
+                        cela.classList = 'suficient';
+                    } else if (user[i] > 5) {
+                        cela.classList = 'aprovat';
+                    }
+                }
+                fila.appendChild(cela);
+            }
+            //taula.innerHTML += `< td > ${ user[0] }</ ><td>${user[1]}</td><td>${user[2]}</td><td>${user[3]}</td>`;
+            taula.appendChild(fila);
         });
+        taula.innerHTML += '</tbody>'
+
     });
 
 
     addUserBtn.addEventListener('click', () => {
-        store.dispatch({ type: "ADD_USER", username: userInput.value });
+        store.dispatch({ type: "ADD_USER", nom: userInput.value, curs: "DAW2", modul: "M06", nota: Math.floor(Math.random() * 11) });
     })
 }
