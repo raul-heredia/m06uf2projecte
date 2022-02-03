9aaafb221e1965a3b6d7d4c036ae50e697d241b8
Raúl Heredia <raul.heredia01@outlook.com>
2022-02-03 17:56:55 +0100

    Comments

--- index.js
+++ index.js
@@ -3,37 +3,38 @@ window.onload = main;
 function main() {
     const reducer = (state = [], action) => {
         console.log('reducer', state, action);
-        if (action.type === 'ADD_USER') {
-            return [...state, { id: action.id, nom: action.nom, curs: action.curs, nota: action.nota }].sort((a, b) => { return a.id - b.id });
+        // Segons el tipus d'acció el reducer farà una cosa o una altra
+        if (action.type === 'ADD_USER') { // Si l'acció es un ADD_USER afegirem l'usuari nou a l'estat actual, ordenant tot el array d'estat per ID ascenent
+            return [...state, { id: action.id, nom: action.nom, curs: action.curs, nota: action.nota }].sort((a, b) => { return a.id - b.id }); // Retornem l'estat amb el nou usuari
         };
-        if (action.type === 'MODIFY_USER') {
-            let objectPos = state.map((x) => { return x.id; }).indexOf(action.id);
-            if (objectPos !== -1) {
-                if (action.nom != "") {
-                    state[objectPos].nom = action.nom;
+        if (action.type === 'MODIFY_USER') { // Si el tipus es MODIFY_USER
+            let objectPos = state.map((x) => { return x.id; }).indexOf(action.id); // Comprovem si existeix l'usuari
+            if (objectPos !== -1) { // Si existeix l'usuari
+                if (action.nom != "") { // Si el camp nom no està buit el canviem, si està buit no canviarà.
+                    state[objectPos].nom = action.nom; // canviem el nom que està guardat en la posició ObjectPos de l'estat
                 }
-                if (action.curs != "") {
-                    state[objectPos].curs = action.curs;
+                if (action.curs != "") { // Si el camp curs no està buit el canviem, si està buit no canviarà.
+                    state[objectPos].curs = action.curs; // canviem el curs que està guardat en la posició ObjectPos de l'estat
                 }
-                if (!isNaN(action.nota)) {
-                    state[objectPos].nota = action.nota;
+                if (!isNaN(action.nota)) { // Si el camp nota es un número el canviem, si no es un número no canviarà.
+                    state[objectPos].nota = action.nota; // canviem la nota que està guardada en la posició ObjectPos de l'estat
                 }
-            } else {
+            } else { // Si l'objecte no existeix enviem un alert
                 alert(`Error, no existeix cap alumne amb el identificador ${action.id}.`);
             }
         };
-        if (action.type === 'DELETE_USER') {
-            let objectPos = state.map((x) => { return x.id; }).indexOf(action.id);
-            if (objectPos !== -1) {
-                state.splice(objectPos, 1);
-            } else {
+        if (action.type === 'DELETE_USER') { // Si la acció es DELETE_USER
+            let objectPos = state.map((x) => { return x.id; }).indexOf(action.id); // Busquem l'usuari a l'array d'estat
+            if (objectPos !== -1) { // si existeix
+                state.splice(objectPos, 1); // fem un splice per a esborrar-lo i que no deixi una posició buida a l'array
+            } else { // Si no existeix donem un error
                 alert(`Error, no existeix cap alumne amb el identificador ${action.id}.`);
             }
         }
-        if (action.type === 'DELETE_USER') {
-            return state.sort((a, b) => { return a.id - b.id });
+        if (action.type === 'RESET_FILTER') { // Si arriba un RESET_FILTER 
+            return state.sort((a, b) => { return a.id - b.id }); // retornem el array normal , no faria falta tenir-lo ja que en cas de que no existís quan arribés el RESET_FiLTER seria retornat al return de la linia 37
         }
-        return state.sort((a, b) => { return a.id - b.id });
+        return state.sort((a, b) => { return a.id - b.id }); // Si no cumpleix cap tipus d'acció retorna l'estat ordenat per id
     };
     const store = Redux.createStore(reducer);
     const taula = document.getElementById('taula');
@@ -51,6 +52,8 @@ function main() {
     let selectMitjana = document.getElementById('selectMitjana');
     let inputFiltrar = document.getElementById('filtrar');
     let mitjana = "tots";
+
+
     const DB_VERSION = 19;
     window.indexedDB = window.indexedDB || window.mozIndexedDB || window.webkitIndexedDB || window.msIndexedDB;
     window.IDBTransaction = window.IDBTransaction || window.webkitIDBTransaction || window.msIDBTransaction || {
@@ -95,10 +98,10 @@ function main() {
             let cursor = event.target.result;
             if (cursor) {
                 //console.log(cursor.key + " es " + cursor.value.nom);
-                store.dispatch({
+                store.dispatch({ // Afegim els usuaris de indexeddb enviant un dispatch de ADD_USER
                     type: "ADD_USER", id: parseInt(cursor.key), nom: cursor.value.nom, curs: cursor.value.curs, nota: parseInt(cursor.value.nota)
                 });
-                WebStorageEmmagatzematge.desar(parseInt(cursor.key), cursor.value.nom)
+                WebStorageEmmagatzematge.desar(parseInt(cursor.key), cursor.value.nom) // Guardem el key i el nom a local storage
                 cursor.continue();
             }
         };
@@ -131,53 +134,53 @@ function main() {
 
     // FUNCIONS
     function afegirUsuari(id, nom, curs, nota) {
-        let objectPos = store.getState().map((x) => { return x.id; }).indexOf(parseInt(id));
-        if (objectPos === -1) {
+        let objectPos = store.getState().map((x) => { return x.id; }).indexOf(parseInt(id)); // Busquem si l'usuari ja existeix
+        if (objectPos === -1) { // Si noe existeix l'afegim
             Emmagatzematge.desar(parseInt(id), nom, curs, parseInt(nota));
             WebStorageEmmagatzematge.desar(parseInt(id), nom);
-            store.dispatch({
+            store.dispatch({ // Fem un dispatch per enviar l'acció de tipus ADD_USER al store de redux que després passarà al reducer
                 type: "ADD_USER", id: parseInt(id), nom: nom, curs: curs, nota: parseInt(nota)
             });
-        } else {
+        } else { // Si existeix no l'afegim i enviem una alerta
             alert(`Error, ja existeix un alumne amb el identificador ${id}.`);
             netejaCamps()
         };
     }
-    function netejaCamps() {
+    function netejaCamps() { // Netejem els camps de input
         idInput.value = '';
         userInput.value = '';
         cursInput.value = '';
         notaInput.value = '';
     }
-    function esborraTaula() {
+    function esborraTaula() { // Esborrem les row de la taula a partir de la número 1, per a no esborrar el header
         while (taula.rows.length > 1) {
             taula.deleteRow(1);
         }
     }
     function generaTaula() {
-        netejaCamps()
-        esborraTaula()
-        store.getState().forEach(user => {
-            let fila = taula.insertRow(-1);
-            fila.insertCell(0).innerHTML = user.id;
-            fila.insertCell(1).innerHTML = user.nom;
-            fila.insertCell(2).innerHTML = user.curs;
-            let nota = fila.insertCell(3);
+        netejaCamps() // Netejem els camps
+        esborraTaula() // Esborrem la taula
+        store.getState().forEach(user => { // Obtenim el estat amb store.getState i iterem per cada objecte que conté
+            let fila = taula.insertRow(-1); // amb insertRow(-1) afegim cada linia al final
+            fila.insertCell(0).innerHTML = user.id; // A la cel·la 0 afegim el id
+            fila.insertCell(1).innerHTML = user.nom; // A la cel·la 1 afegim el nom
+            fila.insertCell(2).innerHTML = user.curs; // A la cel·la 2 afegim el curs
+            let nota = fila.insertCell(3); // Fem una variable per a la cel·la 3
             if (user.nota < 5) {
-                nota.className = 'suspes';
+                nota.className = 'suspes'; // Si la nota es menor a 5 afegim la classe suspes a la cel·la
             } else if (user.nota == 5) {
-                nota.className = 'suficient';
+                nota.className = 'suficient'; // Si la nota es igual a 5 afegim la classe suficient a la cel·la
             } else if (user.nota > 5) {
-                nota.className = 'aprovat';
+                nota.className = 'aprovat'; // Si la nota es major a 5 afegim la classe aprovat a la cel·la
             }
-            nota.innerHTML = user.nota;
-            let esborrar = fila.insertCell(4);
-            esborrar.innerHTML = '<i class="bi bi-x-lg"></i>';
-            esborrar.className = "esborrar";
-            esborrar.setAttribute('id', user.id);
+            nota.innerHTML = user.nota; // Afegim la nota a la cel·la
+            let esborrar = fila.insertCell(4); // Creem una variable per a la cel·la d'esborrar
+            esborrar.innerHTML = '<i class="bi bi-x-lg"></i>'; // Afegim el html per a la icona d'esborrar
+            esborrar.className = "esborrar"; // Afegim la classe esborrar a la cel·la
+            esborrar.setAttribute('id', user.id); // Afegim l'id de usuari a la cel·la
         })
     }
-    function generaTaulaFiltre(arrfilter) {
+    function generaTaulaFiltre(arrfilter) { // Fa el mateix que generaTaula pero li passem un array ja filtrat
         netejaCamps()
         esborraTaula()
         arrfilter.forEach(user => {
@@ -240,38 +243,41 @@ function main() {
 
     addUserBtn.addEventListener('click', () => {
         if (idInput.value !== "" || userInput.value !== "" || cursInput.value !== "" || notaInput.value !== "") {
+            // Si els camps no estàn buits truquem a la funció afegirUsuari
             afegirUsuari(idInput.value, userInput.value, cursInput.value, notaInput.value)
-        } else {
+        } else { // Si falta algun camp fem un alert
             alert("Error, falten camps per omplir.");
         };
     });
     editUserBtn.addEventListener('click', () => {
+        // Quan fem clic al boto d'editar fem un dispatch de tipus MODIFY_USER
         store.dispatch({
             type: "MODIFY_USER", id: parseInt(idInput.value), nom: userInput.value, curs: cursInput.value, nota: parseInt(notaInput.value)
         });
+        // cridem a la funció netejaCamps
         netejaCamps();
     })
     taula.addEventListener('click', (event) => {
-        if (event.target.className == "esborrar" || event.target.parentNode.className == "esborrar") {
-            buttonClicked = event.target.id;
+        if (event.target.className == "esborrar" || event.target.parentNode.className == "esborrar") { // si el class del que hem clicat o el parent node del que hem clicat es esborrar entra al if
+            let buttonClicked = event.target.id; // afegim el id a la variable buttonClicked
             if (event.target.tagName == 'I' && event.target.parentNode.tagName == 'TD') {
-                buttonClicked = event.target.parentNode.id; // Nos aseguramos que clicando el icono no devuelva undefined diciendole que el value es el valor del parentNode (El valor de cada botón)
+                buttonClicked = event.target.parentNode.id; // En cas de que cliquem just la icona, ens asegurem de que no retorni un undefined donant-li el valor del parent node, la classe del TD.
             }
             store.dispatch({
-                type: "DELETE_USER", id: parseInt(buttonClicked)
+                type: "DELETE_USER", id: parseInt(buttonClicked) // Enviem una accio amb dispatch de tipus DELETE_USER
             });
-            Emmagatzematge.esborrarAlumne(buttonClicked)
-            WebStorageEmmagatzematge.esborrarAlumne(parseInt(buttonClicked));
+            Emmagatzematge.esborrarAlumne(buttonClicked) // Esborrem l'usuari de indexeddb
+            WebStorageEmmagatzematge.esborrarAlumne(parseInt(buttonClicked)); // Esborrem la entrada de WebStorage
         }
     });
-    inputFiltrar.addEventListener('keyup', () => {
-        let filterText = store.getState().filter((a) => a.nom.toLowerCase().startsWith(inputFiltrar.value.toLowerCase()) || a.curs.toLowerCase().startsWith(inputFiltrar.value.toLowerCase()))
-        generaTaulaFiltre(filterText);
+    inputFiltrar.addEventListener('keyup', () => { // cada vegada que aixequem una tecla
+        let filterText = store.getState().filter((a) => a.nom.toLowerCase().startsWith(inputFiltrar.value.toLowerCase()) || a.curs.toLowerCase().startsWith(inputFiltrar.value.toLowerCase())) // Generem un array en el qual el nom o el curs començi per el value de inputFiltrar
+        generaTaulaFiltre(filterText); // generem la taula i le passem el array filtrat
     })
     selectMitjana.addEventListener('change', (event) => {
         mitjana = event.target.value;
         let filtre;
-        switch (mitjana) {
+        switch (mitjana) { // fem un array segons si està aprovat o no
             case "aprovats":
                 filtre = store.getState().filter((a) => a.nota >= 5)
                 break;
@@ -279,12 +285,12 @@ function main() {
                 filtre = store.getState().filter((a) => a.nota < 5)
                 break;
         }
-        generaTaulaFiltre(filtre);
+        generaTaulaFiltre(filtre); // generem la taula passant el array filtrat
     });
     resetFilter.addEventListener('click', () => {
-        selectMitjana.getElementsByTagName('option')[0].selected = 'selected'
-        inputFiltrar.value = "";
-        store.dispatch({
+        selectMitjana.getElementsByTagName('option')[0].selected = 'selected'; // resetejem la opció seleccionada de selectMitjana
+        inputFiltrar.value = ""; // Resetejem el value de inputFiltrar
+        store.dispatch({ // Enviem un canvi d'estat al reducer de tipus RESET_FILTER per a que recarregui l'estat i ens retorni la taula original sense filtres
             type: "RESET_FILTER"
         });
     });
